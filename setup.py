ff1702e1233aa9b429089b81b1afc744fb5a1079
Peter Kalverla <peter.kalverla@gmx.com>
2023-05-25 17:05:42 +0200

    add pycaret to dev deps

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     python_requires='>=3.9',
     packages=find_packages(),
     install_requires=["pandas>=1.0", "numpy>=1.20", "scikit-learn", "matplotlib>=3.0", "lightgbm"],
-    extras_require={"dev": ["black", "flake8"]},
+    extras_require={"dev": ["black", "flake8", "pycaret"]},
     include_package_data=True,
     zip_safe=False,
 )
